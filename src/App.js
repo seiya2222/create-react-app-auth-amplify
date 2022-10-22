4d061063cb8fc9903adaa7cc9583889bd4eac326
seiya2222 <seiya2222@gmail.com>
2022-10-22 18:21:52 +0900

    Tutorial

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Sample App</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
